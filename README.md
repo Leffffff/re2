21c6c0a91ae325b90e54e25656181b941dc09c59
lbrekhov <lbrekhov@intersog.com>
2020-06-02 16:44:24 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,57 @@
+# RE2
+
+Copy origin files from google-re2 library
+
+## From re2
+
+``` bash
+cp google-re2/re2/*.cc src
+```
+
+``` bash
+cp google-re2/re2/*.h src
+```
+
+## From util
+
+``` bash
+cp google-re2/util/*.cc src
+```
+
+``` bash
+cp google-re2/util/*.h src
+```
+
+### _In __src/__ global replace from `#include "(?:re2|util)/` to `#include "`_
+
+# emscripten
+
+## Clone git repo in _your repository with projects_
+
+`git clone https://github.com/emscripten-core/emsdk.git`
+
+### Enter that directory
+
+`cd emsdk`
+
+### Fetch the latest version of the emsdk (not needed the first time you clone)
+
+`git pull`
+
+### Download and install the latest SDK tools
+
+`./emsdk install latest`
+
+### Make the "latest" SDK "active" for the current user. (writes ~/.emscripten file)
+
+`./emsdk activate latest`
+
+### Activate PATH and other environment variables in the current terminal
+
+`source ./emsdk_env.sh`
+
+# Runnner
+
+``` bash
+em++ my_test.cc src/re2.cc src/filtered_re2.cc src/prefilter_tree.cc src/regexp.cc src/stringpiece.cc src/unicode_*.cc src/perl_groups.cc src/parse.cc src/rune.cc src/simplify.cc src/compile.cc src/prog.cc src/nfa.cc src/onepass.cc src/prefilter.cc src/dfa.cc src/bitstate.cc src/tostring.cc -o re2.html -s LINKABLE=1 -s EXPORT_ALL=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["stringToUTF8", "UTF8ToString"]'
+```
--- my_test.cc
+++ my_test.cc
@@ -1,38 +1,37 @@
-
-//em++ my_test.cc re2/re2.cc re2/filtered_re2.cc re2/prefilter_tree.cc re2/regexp.cc re2/stringpiece.cc re2/unicode_*.cc re2/perl_groups.cc re2/parse.cc re2/rune.cc re2/simplify.cc re2/compile.cc re2/prog.cc re2/nfa.cc re2/onepass.cc re2/prefilter.cc re2/dfa.cc re2/bitstate.cc re2/tostring.cc -o re2.html -s LINKABLE=1 -s EXPORT_ALL=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["stringToUTF8", "UTF8ToString"]'
-
 #include "src/re2.h"
 #include "src/filtered_re2.h"
 #include <stdio.h>
 #include <string>
 #include <iostream>
 
-//em++ my_test.cc re2/re2.cc re2/filtered_re2.cc re2/prefilter_tree.cc re2/regexp.cc re2/stringpiece.cc re2/unicode_*.cc re2/perl_groups.cc re2/parse.cc re2/rune.cc re2/simplify.cc re2/compile.cc re2/prog.cc re2/nfa.cc re2/onepass.cc re2/prefilter.cc re2/dfa.cc re2/bitstate.cc re2/tostring.cc -o re2.html -s LINKABLE=1 -s EXPORT_ALL=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["stringToUTF8", "UTF8ToString"]'
-
 //em++ my_test.cc src/re2.cc src/filtered_re2.cc src/prefilter_tree.cc src/regexp.cc src/stringpiece.cc src/unicode_*.cc src/perl_groups.cc src/parse.cc src/rune.cc src/simplify.cc src/compile.cc src/prog.cc src/nfa.cc src/onepass.cc src/prefilter.cc src/dfa.cc src/bitstate.cc src/tostring.cc -o re2.html -s LINKABLE=1 -s EXPORT_ALL=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["stringToUTF8", "UTF8ToString"]'
 
-extern "C" {
+extern "C"
+{
 
     // bool stringTestFunc2(char* inputString, char* inputRegex, char** outCapturedArray, int* outCapturedArraySize) {
     //     re2::RE2 regex(inputRegex);
     //     std::cout<< regex.NumberOfCapturingGroups() <<std::endl;
     //     std::string captured;
     //     re2::RE2::FullMatch(text,regex, &captured);
-    //     char* outString = new char[captured.length() + 1]; 
-    //     strcpy(outString, captured.c_str()); 
+    //     char* outString = new char[captured.length() + 1];
+    //     strcpy(outString, captured.c_str());
     //     return outString; /
     // }
 
-    char* stringTestFunc(char* text, char* regex) {
+    char *stringTestFunc(char *text, char *regex)
+    {
         std::string captured;
-        re2::RE2::PartialMatch(text,regex, &captured);
-        char* outString = new char[captured.length() + 1]; 
-        strcpy(outString, captured.c_str()); 
-        return outString; 
+        re2::RE2::PartialMatch(text, regex, &captured);
+        char *outString = new char[captured.length() + 1];
+        strcpy(outString, captured.c_str());
+        return outString;
     }
 
-    bool boolTestFunc(char* text, char* regex) {
-        if(re2::RE2::PartialMatch(text,regex)) {
+    bool boolTestFunc(char *text, char *regex)
+    {
+        if (re2::RE2::PartialMatch(text, regex))
+        {
             printf("PASS\n");
             return true;
         }
